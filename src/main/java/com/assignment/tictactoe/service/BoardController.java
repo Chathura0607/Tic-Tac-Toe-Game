9c136febe60320dbb2d5221f39e5111f6796e093
chathura <chathuhiru45@gmail.com>
2024-10-29 23:57:42 +0530

    UI Done

---
+++ src/main/java/com/assignment/tictactoe/service/BoardController.java
@@ -0,0 +1,13 @@
+package com.assignment.tictactoe.service;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class BoardController {
+    @FXML
+    private Button btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9;
+
+    @FXML
+    void btnOnAction(ActionEvent event) {}
+}
